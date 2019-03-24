49f3ae3bcd0e7befe30478247350d2c7b6d47941
jonathan gerber <jlgerber@gmail.com>
2019-03-23 21:54:05 -0700

    changes getLogger from foo to foo.bar

--- logging_info/__main__.py
+++ logging_info/__main__.py
@@ -6,8 +6,9 @@ logger = logging.basicConfig()
 print "We get the root logger with logging.getLogger()"
 rl = logging.getLogger()
 print "We get a logger named 'foo'"
-fool = logging.getLogger('foo')
+fool = logging.getLogger('foo.bar')
 print "We set the foo logging level to 20"
 fool.level=20
-print "And now we print out the logging hierarchy"
+print "And now we print out the logging hierarchy:"
+print ""
 print_logging_info()
