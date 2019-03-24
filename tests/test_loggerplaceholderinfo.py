1ea549118173ae246723b66ad887795dbab56646
jonathan gerber <jlgerber@gmail.com>
2019-03-23 19:56:11 -0700

    adds test to verify immutability of placeholderinfo handlers and filters

--- tests/test_loggerplaceholderinfo.py
+++ tests/test_loggerplaceholderinfo.py
@@ -11,8 +11,30 @@ class TestBasicLoggerPlaceHolderInfo(unittest.TestCase):
         placeholder_info = logging_info.LoggerPlaceHolderInfo('foo',self.placeholder)
         self.assertEquals(placeholder_info.name, 'foo')
 
-
     def test_init_loggerplaceholderinfo_filters_empty(self):
         placeholder_info = logging_info.LoggerPlaceHolderInfo('foo',self.placeholder)
-        self.assertEquals(placeholder_info.name, 'foo')
         self.assertEqual(len(placeholder_info.filters),0)
+
+    def test_init_loggerplaceholderinfo_handlers_empty(self):
+        placeholder_info = logging_info.LoggerPlaceHolderInfo('foo',self.placeholder)
+        self.assertEqual(len(placeholder_info.handlers),0)
+
+    def test_init_loggerplaceholderinfo_cannot_mutate_handlers(self):
+        placeholder_info = logging_info.LoggerPlaceHolderInfo('foo',self.placeholder)
+        self.assertEqual(len(placeholder_info.handlers), 0)
+        with self.assertRaises(AttributeError) as context:
+            placeholder_info.handlers = ['foo']
+        self.assertTrue("can't set attribute" in context.exception)
+        with self.assertRaises(AttributeError) as context:
+            placeholder_info.handlers.append("foo")
+        self.assertTrue("'frozenset' object has no attribute 'append'")
+
+    def test_init_loggerplaceholderinfo_cannot_mutate_filters(self):
+        placeholder_info = logging_info.LoggerPlaceHolderInfo('foo',self.placeholder)
+        self.assertEqual(len(placeholder_info.handlers), 0)
+        with self.assertRaises(AttributeError) as context:
+            placeholder_info.filters = ['foo']
+        self.assertTrue("can't set attribute" in context.exception)
+        with self.assertRaises(AttributeError) as context:
+            placeholder_info.filters.append("foo")
+        self.assertTrue("'frozenset' object has no attribute 'append'")
