bf1d65c8058b75657f769d075c50105304d53840
jonathan gerber <jlgerber@gmail.com>
2019-03-23 22:31:20 -0700

    formats printing

--- logging_info/__main__.py
+++ logging_info/__main__.py
@@ -1,14 +1,16 @@
 from logging_info import print_logging_info
 import logging
+print "--------------------------"
 print "Example logging_info usage"
-print "We initialize logging using logggin.basicConfig()"
+print "--------------------------"
+print "1 - We initialize logging using loggging.basicConfig()"
 logger = logging.basicConfig()
-print "We get the root logger with logging.getLogger()"
+print "2 - We get the root logger with logging.getLogger()"
 rl = logging.getLogger()
-print "We get a logger named 'foo'"
+print "3 - We get a logger named 'foo.bar'"
 fool = logging.getLogger('foo.bar')
-print "We set the foo logging level to 20"
+print "4 - We set the foo.bar logging level to 20"
 fool.level=20
-print "And now we print out the logging hierarchy:"
-print ""
+print "5 - And now we print out the logging hierarchy:"
+print "--------------------------"
 print_logging_info()
